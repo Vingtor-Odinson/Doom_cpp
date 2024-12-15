2c77f4b19fd5da2edf8ba3648507e92998de43cd
Odinson Vingtor <odinson.vingtor@gmail.com>
2024-12-15 19:48:01 -0300

    Adição do mapLoader e leitura do tipo .map

--- build/CMakeFiles/Doom_cpp.dir/DependInfo.cmake
+++ build/CMakeFiles/Doom_cpp.dir/DependInfo.cmake
@@ -11,6 +11,7 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/home/glauco/Área de trabalho/random/Doom_cpp/src/app.cpp" "CMakeFiles/Doom_cpp.dir/src/app.cpp.o" "gcc" "CMakeFiles/Doom_cpp.dir/src/app.cpp.o.d"
   "/home/glauco/Área de trabalho/random/Doom_cpp/src/engine.cpp" "CMakeFiles/Doom_cpp.dir/src/engine.cpp.o" "gcc" "CMakeFiles/Doom_cpp.dir/src/engine.cpp.o.d"
   "/home/glauco/Área de trabalho/random/Doom_cpp/src/main.cpp" "CMakeFiles/Doom_cpp.dir/src/main.cpp.o" "gcc" "CMakeFiles/Doom_cpp.dir/src/main.cpp.o.d"
+  "/home/glauco/Área de trabalho/random/Doom_cpp/src/mapLoader.cpp" "CMakeFiles/Doom_cpp.dir/src/mapLoader.cpp.o" "gcc" "CMakeFiles/Doom_cpp.dir/src/mapLoader.cpp.o.d"
   "/home/glauco/Área de trabalho/random/Doom_cpp/src/mapRenderer.cpp" "CMakeFiles/Doom_cpp.dir/src/mapRenderer.cpp.o" "gcc" "CMakeFiles/Doom_cpp.dir/src/mapRenderer.cpp.o.d"
   "/home/glauco/Área de trabalho/random/Doom_cpp/src/utils.cpp" "CMakeFiles/Doom_cpp.dir/src/utils.cpp.o" "gcc" "CMakeFiles/Doom_cpp.dir/src/utils.cpp.o.d"
   )
